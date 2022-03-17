d2260fd66701e9c8c9ecba84e9748f27c46f9dae
Jukkraput Pikunyam <jukkraput.p@ku.th>
2022-03-17 14:01:25 +0700

    create db.py & new api: GET users

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from db import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/api/v1/users', methods=['GET'])
+def users():
+    col_ref = db.collection(u'Users')
+    datas = col_ref.get()
+    if datas.exists:
+        return datas
+    else:
+        return 'Users not found', 404
