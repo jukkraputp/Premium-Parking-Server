1f1cc4a24796c970b4d7d9fb01c5685464c378ba
Jukkraput Pikunyam <jukkraput.p@ku.th>
2022-03-20 13:30:06 +0700

    change time zone to GMT+7

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-import datetime
-import json 
+from datetime import datetime, timedelta
+import json
 from flask import Flask, request
 from db import db
 
 month_table = {
-        '01':"Jan",
-        '02':"Feb",
-        '03':"Mar",
-        '04':"Apr",
-        '05':"May",
-        '06':"Jun",
-        '07':"Jul",
-        '08':"Aug",
-        '09':"Sep",
-        '10':"Oct",
-        '11':"Nov",
-        '12':"Dec",
-            }
+    '01': "Jan",
+    '02': "Feb",
+    '03': "Mar",
+    '04': "Apr",
+    '05': "May",
+    '06': "Jun",
+    '07': "Jul",
+    '08': "Aug",
+    '09': "Sep",
+    '10': "Oct",
+    '11': "Nov",
+    '12': "Dec",
+}
 
 app = Flask(__name__)
 
-@app.route('/check_in', methods=['GET']) #OK
+
+@app.route('/check_in', methods=['GET'])  # OK
 def check_in():
     can_check_in = 0
-    current_time = datetime.datetime.now() 
+    current_time = datetime.utcnow() + timedelta(hours=7)
     str_time = format(current_time)
-    year, month_index, day = str_time.split()[0].split("-")
-    current_hour = str_time.split()[1].split(":")[0]
-    search_time = day + " " + month_table[month_index] + " " + year + " " + current_hour
+    year = current_time.year
+    month = month_table[current_time.month - 1]
+    day = current_time.day
+    hour = current_time.hour
+    search_time = day + " " + month + " " + year + " " + hour
     doc_ref = db.collection(u'RFID').document(search_time)
-    
+
     user_rfid = request.args.get('RFID')
     doc = doc_ref.get()
     if doc.exists:
@@ -40,11 +43,13 @@ def check_in():
 
     return format(can_check_in)
 
+
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-@app.route('/api/v1/users', methods=['GET']) #OK
+
+@app.route('/api/v1/users', methods=['GET'])  # OK
 def get_users():
     col_ref = db.collection(u'Users')
     docs = col_ref.stream()
@@ -53,7 +58,8 @@ def get_users():
         data.append(doc.to_dict())
     return json.dumps(data)
 
-@app.route('/api/v1/user/<username>', methods=['GET']) #OK
+
+@app.route('/api/v1/user/<username>', methods=['GET'])  # OK
 def get_user(username):
     doc_ref = db.collection(u'Users').document(username)
     doc = doc_ref.get()
@@ -62,11 +68,13 @@ def get_user(username):
     else:
         return 'User not found!', 404
 
-@app.route('/api/v1/free-park', methods=['POST', 'GET']) #OK
+
+@app.route('/api/v1/free-park', methods=['POST', 'GET'])  # OK
 def free_park():
     if request.method == 'POST':
         data = request.get_json()
-        doc_ref = db.collection(u'FreePark').document('Slot' + str(data['slot']))
+        doc_ref = db.collection(u'FreePark').document(
+            'Slot' + str(data['slot']))
         doc_ref.set({
             'available': data['value']
         })
@@ -81,6 +89,7 @@ def free_park():
             if data['available']:
                 available += 1
         return format(available)
-        
+
+
 if __name__ == '__main__':
     app.run(debug=True)
